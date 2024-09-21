ae1eff014cc48b67143fa214ffcbbd6f5086ffc1
Bibhabaripal <palbibhabari@outlook.com>
2024-09-21 17:02:58 +0530

    modified

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include 'connection.php';
 
-$getStudentData=mysqli_query($conn,"SELECT * FROM `student`");
+$getStudentData=mysqli_query($conn,"SELECT * FROM `student_info`");
 while($rowStudentData=mysqli_fetch_array($getStudentData)) {
   //$sl=$rowStudentData['sl'];
   echo "<pre>";
